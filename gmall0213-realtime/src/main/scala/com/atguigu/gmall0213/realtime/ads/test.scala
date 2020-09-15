5cdabe60dee4f9f4c048aeac80430578a0f65162
liuchongyang <835486902@qq.com>
2020-09-15 15:37:48 +0800

    abc

---
+++ gmall0213-realtime/src/main/scala/com/atguigu/gmall0213/realtime/ads/test.scala
@@ -0,0 +1,14 @@
+package com.atguigu.gmall0213.realtime.ads
+
+/**
+ * @author lcy
+ * @create 2020-09-15 15:36
+ */
+object test {
+
+    def main(args: Array[String]): Unit = {
+
+        println("abc")
+    }
+
+}
